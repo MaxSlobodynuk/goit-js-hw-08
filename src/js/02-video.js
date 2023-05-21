26e4dfd8c394370132c9e196d8c01b861ff674c7
Max <slobodynuk.777@gmail.com>
2023-05-21 19:59:27 +0300

    goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,26 +5,11 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
 
-
-player.getCurrentTime().then(function (seconds) {
+player.on('timeupdate', throttle(function ({seconds}) {
     localStorage.setItem('videoplayer-current-time', seconds);
-}).catch(function (error) {
-    console.log(error);
-});
-
-player.play();
-
-player.on('timeupdate', throttle(function () {
-    player.getCurrentTime().then(function (seconds) {
-        localStorage.setItem('videoplayer-current-time', seconds);
-    }).catch(function (error) {
-    console.log(error);
-    });
-}, 1000));
+}));
 
 const currentTime = localStorage.getItem('videoplayer-current-time');
 if (currentTime) {
-    player.setCurrentTime(currentTime).catch(function (error) {
-        console.log(error);
-    });
-}
+    player.setCurrentTime(currentTime)
+    };
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,68 +9,24 @@ const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
 
-emailInput.addEventListener('input', onEmailInput);
-messageInput.addEventListener('input', throttle(onMessageInput, 500));
+// emailInput.addEventListener('input', onEmailInput);
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-// form.addEventListener('input', evt => {
-//     formData[evt.target.name] = evt.target.value;
-//     console.log(formData);
-// });
-
-// populateMessage();
-
-
-// function onMessageInput(evt) {
-//     const message = evt.target.value;
-
-//     localStorage.setItem(STORAGE_KEY, message);
-// }
-
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
-    
-//     evt.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-// }
-
-// function populateMessage() {
-//     const sevedMessage = localStorage.getItem(STORAGE_KEY);
-
-//     if (sevedMessage) {
-//         messageInput.value = sevedMessage;
-//     }
-// }
-
-
-
-form.addEventListener('input', evt => {
+function onFormInput (evt) {
     formData[evt.target.name] = evt.target.value;
     saveFormData();
-    });
+    };
 
     populateForm();
 
-    function onEmailInput(evt) {
-        const email = evt.target.value;
-        formData[evt.target.name] = email;
-        saveFormData();
-    }
-
-    function onMessageInput(evt) {
-        const message = evt.target.value;
-        formData[evt.target.name] = message;
-        saveFormData();
-    }
-
     function onFormSubmit(evt) {
         evt.preventDefault();
-
+        console.log(formData);
         evt.currentTarget.reset();
         localStorage.removeItem(STORAGE_KEY);
         formData.email = '';
         formData.message = '';
-        console.log(formData);
     }
 
     function saveFormData() {
